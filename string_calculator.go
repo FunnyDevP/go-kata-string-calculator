f552a508581296a5c1b19a186927c0545cc99244
PJ <warahchit.ch@outlook.com>
2021-07-06 00:13:58 +0700

    take 1 return 1

--- string_calculator.go
+++ string_calculator.go
@@ -1,5 +1,5 @@
 package string_calculator
 
 func Add(numbers string) int {
-	return 0
+	return 1
 }
--- string_calculator_test.go
+++ string_calculator_test.go
@@ -9,4 +9,7 @@ func TestStringCalculator(t *testing.T) {
 	t.Run("should return 0 when take empty string", func(t *testing.T) {
 		assert.Equal(t, 0,Add(""))
 	})
+	t.Run("should return 1 when take `1` string", func(t *testing.T) {
+		assert.Equal(t, 1,Add("1"))
+	})
 }
