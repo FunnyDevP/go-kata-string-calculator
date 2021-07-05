c1b0ecf0e1cabe4dd7963ebed1ef1bbfd89eca31
PJ <warahchit.ch@outlook.com>
2021-07-06 01:01:04 +0700

    take 1,-2,-3 return error negatives not allowed

--- string_calculator.go
+++ string_calculator.go
@@ -1,21 +1,33 @@
 package string_calculator
 
 import (
+	"errors"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
-func Add(numbers string) int {
+func Add(numbers string) (int,error) {
 	if len(numbers) == 0 {
-		return 0
+		return 0,nil
 	}
 
 	result := 0
-	re := regexp.MustCompile(`(?m)[0-9]+`)
+	re := regexp.MustCompile(`(?m)[0-9]+|-\d+`)
 	numbersArr := re.FindAllString(numbers,-1)
+
+	negativeVal := make([]string,0)
 	for _, v := range numbersArr {
 		n, _ := strconv.Atoi(v)
+
+		if n < 0 {
+			negativeVal = append(negativeVal,v)
+		}
 		result += n
 	}
-	return result
+
+	if len(negativeVal) > 0 {
+		return 0, errors.New("error: negatives not allowed: " + strings.Join(negativeVal," "))
+	}
+	return result,nil
 }
--- string_calculator_test.go
+++ string_calculator_test.go
@@ -7,25 +7,52 @@ import (
 
 func TestStringCalculator(t *testing.T) {
 	t.Run("should return 0 when take empty string", func(t *testing.T) {
-		assert.Equal(t, 0,Add(""))
+		result, err := Add("")
+
+		assert.Nil(t, err)
+		assert.Equal(t, 0,result)
 	})
 	t.Run("should return 1 when take `1` string", func(t *testing.T) {
-		assert.Equal(t, 1,Add("1"))
+		result, err := Add("1")
+
+		assert.Nil(t, err)
+		assert.Equal(t, 1,result)
 	})
 	t.Run("should return 2 when take `2` string", func(t *testing.T) {
-		assert.Equal(t, 2,Add("2"))
+		result,err := Add("2")
+
+		assert.Nil(t, err)
+		assert.Equal(t, 2,result)
 	})
 	t.Run("should return 3 when take `1,2` string", func(t *testing.T) {
-		assert.Equal(t, 3,Add("1,2"))
+		result, err := Add("1,2")
+
+		assert.Nil(t, err)
+		assert.Equal(t, 3,result)
 	})
 	t.Run("should return 55 when take `1,2,3,4,5,6,7,8,9,10`", func(t *testing.T) {
-		assert.Equal(t, 55,Add("1,2,3,4,5,6,7,8,9,10"))
+		result, err := Add("1,2,3,4,5,6,7,8,9,10")
+
+		assert.Nil(t, err)
+		assert.Equal(t, 55,result)
 	})
 	t.Run("should return 6 when take `1\n2,3`", func(t *testing.T) {
-		assert.Equal(t, 6,Add("1\n2,3"))
+		result, err := Add("1\n2,3")
+
+		assert.Nil(t, err)
+		assert.Equal(t, 6,result)
 	})
 	t.Run("should return 3 when take `//;\n1;2`", func(t *testing.T) {
+		result, err := Add("//;\n1;2")
 
-		assert.Equal(t, 3,Add("//;\n1;2"))
+		assert.Nil(t, err)
+		assert.Equal(t, 3,result)
+	})
+	t.Run("should return error negative now allowed when take `1,-2,-3`", func(t *testing.T) {
+		result, err := Add("1,-2,-3")
+
+		assert.Zero(t, result)
+		assert.NotNil(t, err)
+		assert.Equal(t, "error: negatives not allowed: -2 -3",err.Error())
 	})
 }
