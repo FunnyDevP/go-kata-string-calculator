fed3be690e7fd835c15ea90f7c004b525a678b06
PJ <warahchit.ch@outlook.com>
2021-07-06 00:19:55 +0700

    take 1,2,3,4,5,6,7,8,9,10 return 55

--- string_calculator_test.go
+++ string_calculator_test.go
@@ -18,4 +18,7 @@ func TestStringCalculator(t *testing.T) {
 	t.Run("should return 3 when take `1,2` string", func(t *testing.T) {
 		assert.Equal(t, 3,Add("1,2"))
 	})
+	t.Run("should return 55 when take `1,2,3,4,5,6,7,8,9,10`", func(t *testing.T) {
+		assert.Equal(t, 55,Add("1,2,3,4,5,6,7,8,9,10"))
+	})
 }
