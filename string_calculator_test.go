6d1f11ed4d9707d43b7ea5bcb84c95b98159171b
PJ <warahchit.ch@outlook.com>
2021-07-06 01:05:19 +0700

    test case multiple longer-length separators

--- string_calculator_test.go
+++ string_calculator_test.go
@@ -67,4 +67,10 @@ func TestStringCalculator(t *testing.T) {
 		assert.Nil(t, err)
 		assert.Equal(t, 6,result)
 	})
+	t.Run("should return 6 when take `//[*][%]\\n1*2%3`", func(t *testing.T) {
+		result, err := Add("//[*][%]\\n1*2%3")
+
+		assert.Nil(t, err)
+		assert.Equal(t, 6,result)
+	})
 }
